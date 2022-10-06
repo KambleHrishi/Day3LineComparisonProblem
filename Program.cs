fda7ddfb0028fe799e85382aee49f187c221b712
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-06 23:42:10 +0530

    line lenght calculation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("Welcome To Line Comparison Computation Program");
+            //Console.WriteLine("Welcome To Line Comparison Computation Program");
+            Console.WriteLine("Calculate length of line");
+            UC1_LineCalculation.LineCalculation();
         }
     }
 }
---
+++ UC1_LineCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3LineComparisonProblem
+{
+    internal class UC1_LineCalculation
+    {
+        public static void LineCalculation()
+        {
+            Console.WriteLine("enter x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            var Line = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(Line);
+        }
+    }
+}
