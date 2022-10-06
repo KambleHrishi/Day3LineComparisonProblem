47430faf478031d6c552c5668e6885b1f01c9d4e
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-06 23:48:10 +0530

    Checking if lines are equal

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@
         public static void Main(string[]args)
         {
             //Console.WriteLine("Welcome To Line Comparison Computation Program");
-            Console.WriteLine("Calculate length of line");
-            UC1_LineCalculation.LineCalculation();
+            //Console.WriteLine("Calculate length of line");
+            //UC1_LineCalculation.LineCalculation();
+            Console.WriteLine("Calculate lenghts of two lines and check them if they are equal");
+            UC2_TwoLines.TwoLines();
         }
     }
 }
---
+++ UC2_TwoLines.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3LineComparisonProblem
+{
+    internal class UC2_TwoLines
+    {
+        public static void TwoLines()
+        {
+            Console.WriteLine("enter values for line 1");
+            Console.WriteLine("enter x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            var Line1 = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("length of line 1: " + Line1);
+            Console.WriteLine("------------------------");
+
+
+            Console.WriteLine("enter values for line 2");
+            Console.WriteLine("enter x3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y3");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter x4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y4");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            var Line2 = Math.Sqrt((Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2)));
+            Console.WriteLine("Length of line 2: " + Line2);
+            Console.WriteLine("------------------------");
+
+            if (Line1 == Line2)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+        }
+    }
+}
