9f0ff1025d8579645c598a2b7cd87ddc1a64b910
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-06 23:55:03 +0530

    checking which line is greater

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@
             //Console.WriteLine("Welcome To Line Comparison Computation Program");
             //Console.WriteLine("Calculate length of line");
             //UC1_LineCalculation.LineCalculation();
-            Console.WriteLine("Calculate lenghts of two lines and check them if they are equal");
-            UC2_TwoLines.TwoLines();
+            //Console.WriteLine("Calculate lenghts of two lines and check them if they are equal");
+            //UC2_TwoLines.TwoLines();
+            Console.WriteLine("Comparing two lines and checking which one is greater");
+            UC3_LineComparison.LineComparison();
         }
     }
 }
